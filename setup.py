920b441823e91559e6607a383aa6c4418d53da8c
Na2Cr2O7 <148955755+Na2Cr2O7@users.noreply.github.com>
2025-05-29 19:31:27 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='togaAudioPlayer',
+    version='0.1.0',
+    description='A simple audio player widget for the Toga widget toolkit',
+    author='234568976543546576543',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'toga',
+    ],
+)
